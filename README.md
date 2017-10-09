1781b6158e88803afcbf4868161c8d3378be2780
Marius Soutier <spybreak@gmx.net>
2017-10-09 14:31:01 +0200

    Add include and exclude filters

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Add this to `plugins.sbt`:
 
 ```
 resolvers += Resolver.bintrayIvyRepo("mariussoutier", "sbt-plugins")
-addSbtPlugin("com.mariussoutier.sbt" % "sbt-simple-avrohugger" % "0.9.0")
+addSbtPlugin("com.mariussoutier.sbt" % "sbt-simple-avrohugger" % "0.9.1")
 ```
 
 ## Usage
@@ -21,7 +21,7 @@ First, enable the plugin on the project you want to use it with.
 lazy val root = project(...)
   .enablePlugins(com.mariussoutier.sbt.AvroGenerator)
   .settings(
-    `sourceDirectory in AvroGeneratorKeys.Avro := target.value / "avro" `
+    sourceDirectory in AvroGeneratorKeys.Avro := target.value / "avro"
   )
 ```
 
@@ -29,4 +29,10 @@ Then use `generateSpecific` by adding it to a generator, for example:
 
 ```scala
 sourceGenerators in Compile += Def.sequential(UnpackKeys.unpackJars, AvroGeneratorKeys.generateSpecific)
+```
+
+Filter files by using either the include or exclude filter.
+
+```scala
+includeFilter in Avro := "*.avdl" | "*.avsc"
 ```
--- src/main/scala/com/mariussoutier/sbt/AvroGenerator.scala
+++ src/main/scala/com/mariussoutier/sbt/AvroGenerator.scala
@@ -17,26 +17,31 @@ object AvroGenerator extends AutoPlugin {
   def generateCaseClasses(generator: Generator,
                           srcDir: File,
                           target: File,
-                          log: Logger): Set[java.io.File] = {
+                          log: Logger,
+                          includeFilter: FileFilter,
+                          excludeFilter: FileFilter): Set[java.io.File] = {
 
-    for (inFile <- AvscFileSorter.sortSchemaFiles((srcDir ** "*.avsc").get)) {
-      log.info("Compiling AVSC %s".format(inFile))
-      generator.fileToFile(inFile, target.getPath)
+    def includeFile(file: File): Boolean =
+      includeFilter.accept(file) || !excludeFilter.accept(file)
+
+    for (avscFile <- AvscFileSorter.sortSchemaFiles((srcDir ** "*.avsc").get) if includeFile(avscFile)) {
+      log.info("Compiling AVSC %s".format(avscFile))
+      generator.fileToFile(avscFile, target.getPath)
     }
 
-    for (idl <- AvdlFileSorter.sortSchemaFiles((srcDir ** "*.avdl").get)) {
-      log.info("Compiling Avro IDL %s".format(idl))
-      generator.fileToFile(idl, target.getPath)
+    for (avdlFile <- AvdlFileSorter.sortSchemaFiles((srcDir ** "*.avdl").get) if includeFile(avdlFile)) {
+      log.info("Compiling Avro IDL %s".format(avdlFile))
+      generator.fileToFile(avdlFile, target.getPath)
     }
 
-    for (inFile <- (srcDir ** "*.avro").get) {
-      log.info("Compiling Avro datafile %s".format(inFile))
-      generator.fileToFile(inFile, target.getPath)
+    for (avroFile <- (srcDir ** "*.avro").get if includeFile(avroFile)) {
+      log.info("Compiling Avro datafile %s".format(avroFile))
+      generator.fileToFile(avroFile, target.getPath)
     }
 
-    for (protocol <- (srcDir ** "*.avpr").get) {
-      log.info("Compiling Avro protocol %s".format(protocol))
-      generator.fileToFile(protocol, target.getPath)
+    for (protocolFile <- (srcDir ** "*.avpr").get if includeFile(protocolFile)) {
+      log.info("Compiling Avro protocol %s".format(protocolFile))
+      generator.fileToFile(protocolFile, target.getPath)
     }
 
     (target ** ("*.java"|"*.scala")).get.toSet
@@ -51,9 +56,13 @@ object AvroGenerator extends AutoPlugin {
         new Generator(SpecificRecord),
         (sourceDirectory in Avro).value,
         (sourceManaged in Compile).value / "avro",
-        streams.value.log
+        streams.value.log,
+        (includeFilter in Avro).value,
+        (excludeFilter in Avro).value
       ).toSeq
-    }
+    },
+    includeFilter in Avro := AllPassFilter,
+    excludeFilter in Avro := NothingFilter
   )
 }
 
